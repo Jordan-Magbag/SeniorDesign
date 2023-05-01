746accd24f16ed7ee462b62636785669c5d3a657
Tilo G <Atilano (Tilo) Garcia>
2023-05-01 15:25:02 -0500

    Final updated script

--- CapstoneProjectPlantModel/ConstantScript2.m
+++ CapstoneProjectPlantModel/ConstantScript2.m
@@ -11,8 +11,8 @@ load_system('BasicPumpPlant.slx');
 if ~isfile("pump_constants.mat")
 
     % Desired range of target pressures and target flows
-    input_speed_current = 10:500:6000; % psi (example: 10 to 400 psi, in steps of 10)
-    input_DSOutletArea = 0:.5:5; % m^2 (example: 0 to 800 m^2, in steps of 10) 2.5" plumbing - Area is 4.91
+    input_speed_current = 1000:1000:6000; % psi (example: 10 to 400 psi, in steps of 10)
+    input_DSOutletArea = (((0:.1:2)/39.37).^2 * pi) / 4; % m^2 (example: 0 to 800 m^2, in steps of 10) 2.5" plumbing - Area is 4.91 
 
     % Initialize matrix to store results (rows: target pressures, columns: target flows)
     speed_results = zeros(length(input_speed_current), length(input_DSOutletArea));
@@ -26,6 +26,13 @@ if ~isfile("pump_constants.mat")
     total_entries = (length(input_speed_current)) * (length(input_DSOutletArea));
     f = waitbar(0, "Simulating...");
     for i = 1:length(input_speed_current)
+        set_param('BasicPumpPlant/TS_Constant', 'Value', num2str(input_speed_current(i)));
+        set_param('BasicPumpPlant/TDSOA_Constant', 'Value', num2str(0));
+            % Run the Simulink model
+        sim('BasicPumpPlant.slx');
+    
+        % Record the resulting pressure from the SolutionPumpPlantModel at zero flow
+        dead_head_pressure_result = PumpPressureModel_psi.signals.values(end);
         for j = 1:length(input_DSOutletArea)
             % Print a progress bar so we know how far we are from
             % completing data collection
@@ -43,16 +50,8 @@ if ~isfile("pump_constants.mat")
             speed_results(i, j) = ShaftSpeedModel_rpm.signals.values(end);
             pressure_results(i, j) = PumpPressureModel_psi.signals.values(end);
             flow_results(i, j) = PumpFlowModel_gpm.signals.values(end);
-%         end
-%     end
-%     waitbar(1,f,'Finishing');
-%     pause(1)
-%     close(f)
 
-
-% Find the dead head pressure - Scalar (I think this equation is wrong but I'm not sure what it should be)
-% Dead head pressure is with target flow == 0, So should this 
-        dead_head_pressure(i, j) = 6.89476 * PumpPressureConstant / (pressure_results(i,1)^2);
+            dead_head_pressure(i,j) = dead_head_pressure_result;
 
         end
     end
@@ -64,18 +63,27 @@ else
     load("pump_constants.mat");
 end
 % Calculate the pressure drop (matrix values at each target pressure/flow)
-% **** Should these be flipped? ****
 pressure_drop = dead_head_pressure - pressure_results;
+
+%calc oressure coeff for each dead head pressure, use polyfit, and take an average
 
 % Fit a polynomial to the pressure drop data
 % Order 10 is the only thing that gets a non-negative PumpACd (Red Flag)
 poly_coeff = polyfit(flow_results, pressure_drop, 2); % Quadratic polynomial fit
-Pressure_coeff = poly_coeff(3) ./ (speed_results.^2); % Use 
+Pressure_coeff = poly_coeff(3) ./ (speed_results.^2); % Use     
 
 % Calculate the PumpACdConstant
-%  PumpACdConstant = 1 / sqrt(poly_coeff(2));
-PumpACdConstant = 1 ./ sqrt(Pressure_coeff);
+PumpACdConstant = 1 / sqrt(poly_coeff(1));
+%PumpACdConstant = 1 ./ sqrt(Pressure_coeff); % Issue**
+%display(mean(mean(PumpACdConstant)));
 display(PumpACdConstant);
+
+a = PumpPressureConstant;
+c = (-1*pressure_results) - ((flow_results.^2)./PumpACdConstant);
+
+ImpellerDrag = ((a*(speed_results.^2))+c)/speed_results;
+% ImpellerDrag = 
+display(mean(mean(ImpellerDrag)));
 
 % Use Peterman equation to solve for Impeller Drag Algebraicly
 
@@ -103,7 +111,7 @@ scatter3(InputSpeedVec, InputDSOAVec, FlowResultsVec, 'filled')
 xlabel('Input Speed')
 ylabel('Input DSOA')
 zlabel('Flow Results')
-title('3D Scatter Plot of Pressure Results')
+title('3D Scatter Plot of Flow Results')
 grid on
 
 figure
